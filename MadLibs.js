a167e8fc6607887b165295e8d517d4fe1cf86b11
aiman0427 <43216032+aiman0427@users.noreply.github.com>
2018-11-28 21:21:10 -0600

    Add files via upload

---
+++ MadLibs.js
@@ -0,0 +1,22 @@
+function array1()
+{
+    var words = [];
+    words.push(document.madLib.word1.value);
+    words.push(document.madLib.word2.value);
+    words.push(document.madLib.word3.value);
+    words.push(document.madLib.word4.value);
+    words.push(document.madLib.word5.value);
+    words.push(document.madLib.word6.value);
+    words.push(document.madLib.word7.value);
+    words.push(document.madLib.word8.value);
+    words.push(document.madLib.word9.value);
+    words.push(document.madLib.word10.value);
+    words.push(document.madLib.word11.value);
+    words.push(document.madLib.word12.value);
+
+    return "Fellow students of " + words[11].bold() + "! We the members of the Students for an " + words[1].bold() + " Society are meeting here to decide what " +
+        "action to take about the Dean of " + words[8].bold() + "." + " He has just fired our friend, Professor " + words[0].bold() + " because she wore her " + words[7].bold() +
+        " long, and because she dressed in a " + words[4].bold() + " and wore " + words[9].bold() + "." + " Next week, we are going to protest by taking over the " + words[5].bold() +
+        " buildings and kidnapping the Assistant " + words[6].bold() + "." + " We also will demand that all students have the right to wear " + words[2].bold() + " hair and " +
+        words[3].bold() + " beards. Remember our slogan: Down with " + words[10].bold() + "."
+}
